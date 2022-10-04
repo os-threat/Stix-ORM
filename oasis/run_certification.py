e4fb566967e5e53f1f0f8f607c95e9683529e1cf
priamai <paolo@priam.ai>
2022-10-04 23:20:55 +0100

    Fixed a few malformed JSON test files. There are more to do.

--- oasis/run_certification.py
+++ oasis/run_certification.py
@@ -3,7 +3,7 @@ import logging
 import re
 from stix.module.typedb import TypeDBSink, TypeDBSource
 from dbconfig import connection
-
+from stix2 import (v21, parse)
 from pathlib import Path
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s')
@@ -28,11 +28,12 @@ def load_template(file_path='./oasis/cert_template.txt'):
 def run_profiles(config:dict,template,tags,out_file):
     report = template
     # create the database and init
-    typedb = TypeDBSink(connection, True, "STIX21")
+    sink_db = TypeDBSink(connection=connection, clear=True,import_type= "STIX21")
+    source_db =  TypeDBSource(connection=connection, import_type="STIX21")
 
     for profile in config.keys():
         logger.info(f'Checking profile {profile}')
-        result = run_profile(profile,config[profile])
+        result = run_profile(profile,config[profile],sink_db,source_db)
         logger.info(result)
 
         for code in result.keys():
@@ -43,7 +44,53 @@ def run_profiles(config:dict,template,tags,out_file):
     with open(out_file,'w') as file:
         file.write(report)
 
-def run_profile(short,profile):
+def verify_file(file_path,sink_db):
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        logger.info(f"Loading file {file_path}")
+        json_blob = json.load(file)
+
+        if isinstance(json_blob, list):
+            for item in json_blob:
+                stix_obj = parse(item)
+                sink_db.add(stix_obj)
+                '''
+                return_dict = source_db.get(stix_obj.id)
+                return_obj = parse(return_dict)
+                cmp = StixComparator()
+                check, p_ok, p_not = cmp.compare(stix_obj, return_obj)
+                return check
+                '''
+                return True
+        else:
+            bundle = parse(json_blob)
+            for stix_obj in bundle.objects:
+                sink_db.add(stix_obj)
+                '''
+                return_dict = self._typedbSource.get(stix_obj.id)
+                return_obj = parse(return_dict)
+                cmp = StixComparator()
+                check, p_ok, p_not = cmp.compare(stix_obj, return_obj)
+                logger.info(f'OK properties {p_ok}')
+                logger.info(f'KO properties {p_not}')
+                self.assertTrue(check)
+                '''
+            return True
+
+def verify_files(directory,sink_db,source_db):
+    """ Load and verify the file
+
+    Args:
+        fullname (): folder path
+    """
+    path = Path(directory)
+
+    if path.is_dir():
+        for file_path in path.iterdir():
+            verify_file(file_path,sink_db)
+    else:
+        logger.error('This is not a folder???')
+
+def run_profile(short,profile,sink_db,source_db):
     logger.info(f'Title {profile["title"]}')
     root_folder = './data/stix_cert_data'
     results = {}
@@ -54,7 +101,9 @@ def run_profile(short,profile):
             consumer_passed = True
             producer_passed = True
             test_file = f"{root_folder}/{level['dir']}/{level['sub_dir']}"
-            logger.info(f"Test file {test_file}")
+            logger.info(f"Test folder {test_file}")
+            check = verify_files(test_file,sink_db,source_db)
+
             if level['sub_dir'] == 'consumer_test':
                 check = True
                 if check == False: consumer_passed = False
