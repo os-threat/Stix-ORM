3142adf0a155d840e6cec73b985fc1f4f794f61a
priamai <paolo@priam.ai>
2022-10-17 22:51:19 +0100

    Being script to run oasis test.

--- run_oasis.sh
+++ run_oasis.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # TODO in windows here you should add the STIX folder to your PYTHONPATH
+# TODO also change dbconfig.py in oasis to match your config
 python3 oasis/run_certification.py
