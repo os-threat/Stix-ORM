9cd3fb59e50e51e83b22c216115aa10ad91c0cc4
Denis O'Sullivan <denis@cloudaccelerator.co>
2023-03-08 09:41:24 +1000

    Logging: Added return

--- stix/module/typedb_lib/queries.py
+++ stix/module/typedb_lib/queries.py
@@ -29,8 +29,8 @@ def build_insert_query(layer):
         insert_tql = ''
     else:
         insert_tql = 'insert ' + indep_ql + dep_insert
-    logger.info(f'match_tql string?-> {match_tql}')
-    logger.info(f'insert_tql string?-> \n {insert_tql}')
+    logger.info(f'\n match_tql string?-> {match_tql}')
+    logger.info(f'\n insert_tql string?-> {insert_tql}')
     typeql_string = match_tql + insert_tql
 
     insertion_is_empty = len(insert_tql) == 0
