186a57eef4767db4fc8d47113a57c179b925c35c
Denis O'Sullivan <denis@cloudaccelerator.co>
2023-03-23 10:12:45 +1000

    Fix: Writing to file

--- stix/module/typedb_lib/file.py
+++ stix/module/typedb_lib/file.py
@@ -6,5 +6,6 @@ from returns.io import impure_safe
 @impure_safe
 def write_to_file(file_name: str,
                   obj):
+    json_object = json.loads(obj.serialize())
     with open(file_name, "w") as outfile:
-        json.dump(obj, outfile)
+        json.dump(json_object, outfile)
