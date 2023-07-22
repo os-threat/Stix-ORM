5bd1f8aa4e927aa6781f8e8db9bd8a7bcdef1d5a
Denis O'Sullivan <denis@cloudaccelerator.co>
2023-07-22 20:00:48 +1000

    Actions: Remove some of the logging in pytest

--- test/conftest.py
+++ test/conftest.py
@@ -3,4 +3,4 @@ import logging
 
 def pytest_configure(config):
     # Set up the logging format and level
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.basicConfig(level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s")
