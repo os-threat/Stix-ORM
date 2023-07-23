072b182e0f5df9c01761325bceea5a0483555de7
Denis O'Sullivan <denis@cloudaccelerator.co>
2023-07-23 10:34:54 +1000

    Added failing pytest

--- test/test_mitre.py
+++ test/test_mitre.py
@@ -78,6 +78,11 @@ def ics_attack_data():
 
     return json_text
 
+def validate_has_error(results):
+    for result in results:
+        if result.status == ResultStatus.ERROR:
+            print("Error " + result.id + " " + result.message)
+            raise Exception("failed")
 
 def validate_has_missing_dependencies(results):
     for result in results:
@@ -86,6 +91,13 @@ def validate_has_missing_dependencies(results):
 def validate_is_successful(results):
     for result in results:
         assert result.status in [ResultStatus.SUCCESS]
+
+def enterprise_attack_13_1():
+    top_dir_path = pathlib.Path(__file__).parents[0]
+    file_path = top_dir_path.joinpath("data").joinpath("mitre").joinpath("enterprise-attack-13.1.json")
+    with open(str(file_path), "r") as file:
+        data = json.load(file)
+    return data["objects"]
 
 def attack_data():
     url = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/index.json"
@@ -125,7 +137,7 @@ class TestMitre:
             if result.status not in [ ResultStatus.SUCCESS, ResultStatus.ALREADY_IN_DB]:
                 message = "" if result.message is None else result.message
                 error = "" if result.error is None else result.error
-                logging.info("Unsuccessful results " + result.id + " " + str(result.status) + " " + message + " " + error)
+                logging.warning("Unsuccessful results " + result.id + " " + str(result.status) + " " + message + " " + error)
         for result in results:
             assert result.status in [ ResultStatus.SUCCESS, ResultStatus.ALREADY_IN_DB]
 
@@ -139,13 +151,14 @@ class TestMitre:
         result = typedb.add(json_data)
         validate_is_successful(result)
 
+
     def test_load_enterprise_attack_13_1(self, setup_teardown, typedb):
         top_dir_path = pathlib.Path(__file__).parents[0]
         file_path = top_dir_path.joinpath("data").joinpath("mitre").joinpath("enterprise-attack-13.1.json")
         with open(str(file_path), "r") as file:
             data = json.load(file)
 
-        result = typedb.add(data)
+        result = typedb.add([data["objects"][1831]])
         self.validate_successful_result(result)
 
     @pytest.mark.skip(reason="This will be added later")
