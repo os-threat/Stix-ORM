6ff9e4c236e37fa35fe4773c61be6da6192c170f
Denis O'Sullivan <denis@cloudaccelerator.co>
2023-08-07 00:27:44 +1000

    Logging: Reduce logging

--- stixorm/__init__.py
+++ stixorm/__init__.py
@@ -1,3 +1,3 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s')
+logging.basicConfig(level=logging.WARNING, format='[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s')
