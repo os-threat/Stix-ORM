e35c5a2736dd53b8c24013f0a0643e46cf6f8e6d
priamai <paolo@priam.ai>
2023-08-23 13:28:26 +0100

    Example of write and read data.

--- test/test_load_data.py
+++ test/test_load_data.py
@@ -11,44 +11,71 @@ from stix2 import parse
 from stixorm.module.authorise import import_type_factory
 from stixorm.module.typedb import TypeDBSink,TypeDBSource
 from stixorm.module.typedb_lib.instructions import ResultStatus
+import stix2
 
 import_type = import_type_factory.get_default_import()
 
-def test_campaign_data():
-    url = "https://raw.githubusercontent.com/os-threat/Stix-ORM/main/test/data/standard/campaign.json"
+connection = {
+    "uri": "localhost",
+    "port": "1729",
+    "database": "stixorm",
+    "user": None,
+    "password": None
+}
 
-    response = requests.get(url)
+db_write = TypeDBSink(
+    connection=connection,
+    clear=True,
+    import_type=import_type,
+)
+
+db_read = TypeDBSource(
+    connection=connection,
+    import_type=import_type,
+)
+
+def test_campaign_data():
+    url1 = "https://raw.githubusercontent.com/os-threat/Stix-ORM/main/test/data/standard/aaa_identity.json"
+    url2 = "https://raw.githubusercontent.com/os-threat/Stix-ORM/main/test/data/standard/campaign.json"
+    # Insert the identities first
+    response = requests.get(url1)
+
     if response.status_code==200:
 
-        data = response.json()
+        identities = response.json()
 
-        connection = {
-            "uri": "localhost",
-            "port": "1729",
-            "database": "stixorm",
-            "user": None,
-            "password": None
-        }
-
-        db_write = TypeDBSink(
-            connection=connection,
-            clear=True,
-            import_type=import_type,
-        )
-
-        db_read = TypeDBSource(
-            connection=connection,
-            import_type=import_type,
-        )
-        if type(data)==list:
+        if type(identities)==list:
             print('Ready to load ....')
-            print(data)
 
-            db_write.add(data)
+            inserts = db_write.add(identities)
 
-            for obj in data:
-                ret = db_read.get(stix_id=obj['id'])
+            for result in inserts:
+                assert result.message is None
+                assert result.error is None
+            for obj in identities:
+                found = db_read.get(stix_id=obj['id'])
 
-                print(ret)
+                assert type(found)==stix2.v21.sdo.Identity
+                print("Object was found!")
+
+        # insert the campaigns later
+
+        url2 = "https://raw.githubusercontent.com/os-threat/Stix-ORM/main/test/data/standard/campaign.json"
+
+        response = requests.get(url2)
+
+        if response.status_code == 200:
+            data = response.json()
+            campaigns = []
+            for campaign in data:
+                #fix the identity
+                campaign['created_by_ref']=identities[0]['id']
+                campaigns.append(campaign)
+            # missing dependencies here but why I just inserted the identiy before....
+            inserts = db_write.add(campaigns)
+
+            for result in inserts:
+                assert result.message is None
+                assert result.error is None
 
         db_write.clear_db()
