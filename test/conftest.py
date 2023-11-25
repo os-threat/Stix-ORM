5f725efb0d0ff973b0b2b8d2303027e5563b9c45
Denis O'Sullivan <denis@cloudaccelerator.co>
2023-11-25 10:11:29 +1000

    Fix: Change database name

--- test/conftest.py
+++ test/conftest.py
@@ -94,4 +94,4 @@ def setup_before_all_tests():
     all_databases = get_all_databases(data_base_uri(), database_port())
     for database in all_databases:
         if database.name.startswith(data_base_prefix()):
-            delete_database(data_base_uri(), database_port(), database.name())
+            delete_database(data_base_uri(), database_port(), database.name)
