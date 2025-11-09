1b50aedf2323889d84787b3b348ce5e76f61277b
Denis O'Sullivan <denis@cloudaccelerator.co>
2025-11-09 11:15:08 +1000

    Improvement: Remove unused

--- stixorm/module/typedb_lib/file.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def write_to_file(file_name: str,
-                  obj):
-    json_object = json.loads(obj.serialize())
-    with open(file_name, "w") as outfile:
-        json.dump(json_object, outfile)
